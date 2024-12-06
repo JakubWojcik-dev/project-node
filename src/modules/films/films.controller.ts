72f93ab44a0f8546fbc936d606f30f12c00a2c98
JakubWojcik-dev <Jakub-mail1@o2.pl>
2024-12-06 09:44:11 +0100

    feat: create new films endpoint

--- src/modules/films/films.controller.ts
+++ src/modules/films/films.controller.ts
@@ -1,20 +1,24 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { FilmsService } from './films.service';
 import { Cache } from 'src/db/db.schema';
+import { ResponseData } from 'src/types/app';
 
 @Controller()
 export class FilmsController {
   constructor(private readonly filmsService: FilmsService) {}
 
   @Get('films')
-  async getFilms(
-    @Query('count') page?: string,
-  ): Promise<{ count: number; data: any[] }> {
+  async getFilms(@Query('count') page?: string): Promise<ResponseData> {
     return await this.filmsService.getFilms(Number(page));
   }
 
   @Get('films/:id')
   async getFilmsById(@Param('id') id: string): Promise<Response> {
     return await this.filmsService.getFilmsById(id);
+  }
+
+  @Get('filmsByParams')
+  async getFilmsOpeningCrawl(): Promise<any[]> {
+    return await this.filmsService.getFilmsOpeningCrawl();
   }
 }
